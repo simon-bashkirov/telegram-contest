aec1de00099c38ebf759652214050eb38bf64887
simon-bashkirov <sp-box@yandex.ru>
2019-03-17 19:56:12 +0300

    Added check group.

--- app/src/main/java/com/bashkirov/telegram/contest/ui/views/CompoundChartView.java
+++ app/src/main/java/com/bashkirov/telegram/contest/ui/views/CompoundChartView.java
@@ -1,20 +1,32 @@
 package com.bashkirov.telegram.contest.ui.views;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 
 import com.bashkirov.telegram.contest.R;
 import com.bashkirov.telegram.contest.models.ChartModel;
+import com.bashkirov.telegram.contest.models.CurveModel;
+
+import java.util.List;
 
 public class CompoundChartView extends LinearLayout {
+
+    private ChartModel mChartModel;
 
     //Components
     private SimpleChartView mSimpleChartView;
     private DetailedChartView mDetailedChartView;
     private SeekBar mSeekBar;
+    private LinearLayout mCheckGroup;
 
 
     public CompoundChartView(Context context) {
@@ -30,8 +42,10 @@ public class CompoundChartView extends LinearLayout {
 
     //========= Public methods ===================
     public void loadChart(ChartModel chartModel) {
+        mChartModel = chartModel;
         mSimpleChartView.loadChart(chartModel);
         mDetailedChartView.loadChart(chartModel);
+        setCheckGroup();
     }
 
     //////////////////////////////////////////////////
@@ -39,6 +53,7 @@ public class CompoundChartView extends LinearLayout {
         mSimpleChartView = findViewById(R.id.simple_chart_view);
         mDetailedChartView = findViewById(R.id.rangable_chart_view);
         mSeekBar = findViewById(R.id.seekBar);
+        mCheckGroup = findViewById(R.id.check_group);
         initSeekBarListener();
 
     }
@@ -66,5 +81,35 @@ public class CompoundChartView extends LinearLayout {
             }
         });
 
+    }
+
+    private void setCheckGroup() {
+        int count = 0;
+        List<CurveModel> curves = mChartModel.getCurves();
+        for (CurveModel curve : mChartModel.getCurves()) {
+            String name = curve.getName();
+            int color = curve.getColor();
+            CheckBox checkBox = new CheckBox(getContext());
+            checkBox.setText(name);
+            setCheckBoxTint(checkBox, color);
+            checkBox.setChecked(true);
+            mCheckGroup.addView(checkBox);
+            if (count < curves.size() - 1) {
+                View divider = new View(getContext());
+                divider.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, Math.round(getResources().getDimension(R.dimen.divider_height))));
+                divider.setBackgroundColor(Color.LTGRAY);
+                mCheckGroup.addView(divider);
+                count++;
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void setCheckBoxTint(CheckBox checkBox, int color) {
+        Drawable buttonDrawable = checkBox.getButtonDrawable();
+        if (buttonDrawable != null) {
+            buttonDrawable.setTint(color);
+            checkBox.setButtonDrawable(buttonDrawable);
+        }
     }
 }
